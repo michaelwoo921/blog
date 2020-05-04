1a2d41a94b8bd305dc744f2770e19bdbf48c7f18
Michael <michaelwoo921@gmail.com>
2020-05-03 23:10:41 -0700

    overfetching postsandusers

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,20 @@ import jsonPlaceholder from '../apis/jsonPlaceholder';
 import _ from 'lodash';
 
 
+export const fetchPostsAndUsers=() => async (dispatch, getState) =>{
+    console.log('about to fetch posts');
+    await dispatch(fetchPosts());
+    // const userIds = _.uniq(_.map((getState().posts), 'userId'));
+    
+    // userIds.forEach(id => dispatch(fetchUser(id)));
+    _.chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach(id => dispatch(fetchUser(id)) )
+        .value();
+        
+}
+
 export const fetchPosts = () =>  async dispatch => {
         const response = await jsonPlaceholder.get('/posts');
 
@@ -12,18 +26,18 @@ export const fetchPosts = () =>  async dispatch => {
      
 }
 
-// export const fetchUser =(id) => async dispatch => {
-//     const response = await jsonPlaceholder.get(`/users/${id}`);
-//     dispatch({type: 'FETCH_USER', payload: response.data});
-
-// }
-
-export const fetchUser = (id) => dispatch =>{
-    _fetchUser(id, dispatch);
-}
-
-const _fetchUser = _.memoize(async (id, dispatch)=> {
+export const fetchUser =(id) => async dispatch => {
     const response = await jsonPlaceholder.get(`/users/${id}`);
     dispatch({type: 'FETCH_USER', payload: response.data});
 
-})
+}
+
+// export const fetchUser = (id) => dispatch =>{
+//     _fetchUser(id, dispatch);
+// }
+
+// const _fetchUser = _.memoize(async (id, dispatch)=> {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+//     dispatch({type: 'FETCH_USER', payload: response.data});
+
+// })
